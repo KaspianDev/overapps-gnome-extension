cb4c7784bf0641698dd1c8e3e26bd3f2c093c963
Kaspian <34658474+KaspianDev@users.noreply.github.com>
2022-06-23 22:05:28 +0200

    Allow closing grid & Fix event loop

--- overapps@kacperleaguenine.gmail.com/extension.js
+++ overapps@kacperleaguenine.gmail.com/extension.js
@@ -6,10 +6,22 @@ const Main = imports.ui.main;
 function init() {}
 
 function enable() {
-
+    var press = false;
+    global.stage.connectObject('key-release-event', (a, e) => {
+        if (Main.overview._shown && e.get_key_symbol() === Clutter.KEY_space) {
+            press = false;
+        }
+    });
     global.stage.connectObject('key-press-event', (a, e) => {
-        if (Main.overview._shown && e.get_key_symbol() === Clutter.KEY_space)
-            Main.overview.dash.showAppsButton.set_checked(true);
+        if (Main.overview._shown && e.get_key_symbol() === Clutter.KEY_space) {
+            if (press) return;
+            press = true;
+            if (!Main.overview.dash.showAppsButton.checked) {
+                Main.overview.dash.showAppsButton.set_checked(true);
+            } else {
+                Main.overview.dash.showAppsButton.set_checked(false);
+            }
+        }
     });
 
 }
